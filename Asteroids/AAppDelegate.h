00cb4ced1da61ce247349a80a338dcffbdb27101
Anton Katekov <void0main@gmail.com>
2013-09-04 13:26:01 +0400

    Initial Commit

---
+++ Asteroids/AAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AAppDelegate.h
+//  Asteroids
+//
+//  Created by Anton Katekov on 04.09.13.
+//  Copyright (c) 2013 KatekovAnton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AViewController;
+
+@interface AAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AViewController *viewController;
+
+@end
---
+++ Asteroids/AViewController.m
@@ -0,0 +1,29 @@
+//
+//  AViewController.m
+//  Asteroids
+//
+//  Created by Anton Katekov on 04.09.13.
+//  Copyright (c) 2013 KatekovAnton. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
